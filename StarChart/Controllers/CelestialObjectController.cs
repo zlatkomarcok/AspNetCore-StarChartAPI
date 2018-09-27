33730ad4a135a7b6b9eb857c4ce4490c71e4f6c6
Eric J Fisher <EricJFisher@users.noreply.github.com>
2018-09-27 11:53:28 -0400

    Corrected GetById to include Route Name

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -18,7 +18,7 @@ namespace StarChart.Controllers
             _context = context;
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetById")]
         public IActionResult GetById(int id)
         {
             var celestialObject = _context.CelestialObjects.Find(id);
