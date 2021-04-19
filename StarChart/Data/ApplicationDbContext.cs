fc09efd6c280ba832ea2b1ec0687f0a9d4fee22d
zlatkomarcok <82848779+zlatkomarcok@users.noreply.github.com>
2021-04-19 19:35:11 -0400

    asdf

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StarChart.Models;
 
 namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestialObjects> CelestialObjects { get; set; }
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
---
+++ StarChart/Models/CelestialObjects.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarChart.Models
+{
+    public class CelestialObjects
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int? OrbitedOjbectId { get; set; }
+        [NotMapped]
+        public List<CelestialObjects> Satgellites { get; set; }
+        public TimeSpan OrbitalPeriod { get; set; }
+
+    }
+}
