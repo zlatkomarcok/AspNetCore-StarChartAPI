3e7aa31aadcd74ff3c70e18e558109f1c77ffea7
zlatkomarcok <82848779+zlatkomarcok@users.noreply.github.com>
2021-04-19 19:37:48 -0400

    Update CelestialObject.cs

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -12,9 +12,9 @@ namespace StarChart.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        public int? OrbitedOjbectId { get; set; }
+        public int? OrbitedObjectId { get; set; }
         [NotMapped]
-        public List<CelestialObject> Satgellites { get; set; }
+        public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
 
     }
